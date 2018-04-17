37a0e7802b5356bec46d473721351e61e7e1ae26
Sbarcenas <sbarcenas00@gmail.com>
2018-04-16 21:39:39 -0500

    Add ReactJS

---
+++ src/Open.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section class="Open">
+        <h1>Hola mundo</h1>
+      </section> 
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Open from './Open';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Open />, document.getElementById('root'));
+registerServiceWorker();
